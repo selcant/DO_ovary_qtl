b2d04a5392cba2beeae14e0e4af127f8e96a9b8d
Selcan Aydin <selcant@gmail.com>
2022-01-14 11:29:59 -0500

    adding permutations.

---
+++ get_perm.R
@@ -0,0 +1,24 @@
+#!/usr/bin/env Rscript
+# I want to get permutation results for ovary counts
+
+# load packages
+# .libPaths("/projects/munger-lab/projects/renv/cache/v5/R-4.1/x86_64-pc-linux-gnu/") # add renv library to path
+
+library(qtl2)
+#library(tidyverse)
+
+
+# load prepped data
+load("/projects/munger-lab/projects/DO_ovary_QTL/data/perm_objects.RData")
+
+# get significance thresholding for all
+# # permutations:
+qtl_map_perm_thres <- scan1perm(
+  genoprobs = genoprobs,
+  pheno =   rankz_all_pheno,
+  kinship = kinship_loco,
+  addcovar = covar,
+  n_perm = 1000, 
+  cores = 16
+)
+save( qtl_map_perm_thres, file = "/projects/munger-lab/projects/DO_ovary_QTL/data/perm_thres.RData")
